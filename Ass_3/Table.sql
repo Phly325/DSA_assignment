ed1d2a75d75129adfc672f5f74ba43cc907533dd
Marvel-1001 <74077826+Marvel-1001@users.noreply.github.com>
2021-11-15 00:05:12 +0200

    Add files via upload

---
+++ Ass_3/Table.sql
@@ -0,0 +1,15 @@
+
+-- create database
+CREATE DATABASE ComputingAndInformatics;
+
+
+CREATE TABLE CourseOutline(
+ Id INT PRIMARY KEY AUTO_INCREMENT,
+ courseInformation JSON,
+ LeactureInformation JSON,
+ LearningOutcomes JSON,
+ CourseContent JSON,
+);
+
+
+
